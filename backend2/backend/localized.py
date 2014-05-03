3eb1fe94531da671a896c9797e4835239cf350a5
nate-parrott <nateparro2t@gmail.com>
2014-05-03 14:49:18 -0400

    gjetgpe

--- backend2/backend/localized.py
+++ backend2/backend/localized.py
@@ -8,6 +8,6 @@ def localized_message(message_dict, user):
     preferred_langs = user.get('prefs', {}).get('languages', ['en'])+['en']
     for lang in preferred_langs:
         if lang in message_dict:
-            return message_dict['lang']
+            return message_dict[lang]
     return message_dict.values()[0]
     
